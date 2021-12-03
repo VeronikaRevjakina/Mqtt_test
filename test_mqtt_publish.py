35aed9697d2b4589d4bf3f2f5c3832bd44d03399
Veronika Revyakina <veronika.revyakina@syngenta.com>
2021-12-03 12:51:45 +0300

    Implemented json parsing, but with 2 separate dbirth and ddata files

--- test_mqtt_publish.py
+++ test_mqtt_publish.py
@@ -1,12 +1,15 @@
 import os
-import random
 import sys
 import time
 import logging
+import json
 
 import paho.mqtt.client as mqtt
 from Sparkplug.client_libraries.python import sparkplug_b
 from Sparkplug.client_libraries.python.sparkplug_b import *
+
+DBIRTH_FILE_NAME = 'dbirth'
+DDATA_FILE_NAME = 'ddata'
 
 sys.path.insert(0, "../client_libraries/python/")
 
@@ -16,9 +19,17 @@ topic = os.environ['TOPIC']
 logger = logging.getLogger()
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-
-def _generate_bool():
-    return random.choice([True, False])
+# Matching json field to sparkplug MetricDataType
+data_type_matching = {
+    'Int8': MetricDataType.Int8,
+    'Int16': MetricDataType.Int16,
+    'Int32': MetricDataType.Int32,
+    'Int64': MetricDataType.Int64,
+    'Float': MetricDataType.Float,
+    'Double': MetricDataType.Double,
+    'Boolean': MetricDataType.Boolean,
+    'String': MetricDataType.String,
+}
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -37,34 +48,29 @@ def on_message(mqttc, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 
-def publishDeviceBirth():
+def publish_device_birth(dbirth):
     print("Publishing Device Birth")
 
     # Get the payload
     payload = sparkplug_b.getDeviceBirthPayload()
 
     # Add some device metrics
-    addMetric(payload, "STA04_Stacking/Stacking/Cylinders/Track 4 -Lifting cylinder/Start to work positionoutput", 154,
-              MetricDataType.Boolean, False)
-    addMetric(payload,
-              "STA02_Lens Placing/SuctionGrippers/Cylinders/Track 2 - Moving to the right/ERROR - Work positionbusy",
-              155, MetricDataType.Boolean, False)
+    __add_metric_from_json(payload, dbirth)
 
     # Publish the initial data with the Device BIRTH certificate
-    totalByteArray = bytearray(payload.SerializeToString())
-    client.publish(topic, totalByteArray, 0, False)
+    total_byte_array = bytearray(payload.SerializeToString())
+    client.publish(topic, total_byte_array, 0, False)
 
 
-def publishDData():
+def publish_ddata(ddata):
     while True:
-        inboundPayload = sparkplug_b.getDdataPayload()
-        addMetric(inboundPayload, "", 154, MetricDataType.Boolean, _generate_bool())
-        addMetric(inboundPayload, "", 155, MetricDataType.Boolean, _generate_bool())
-        byteArray = bytearray(inboundPayload.SerializeToString())
+        inbound_payload = sparkplug_b.getDdataPayload()
+        __add_metric_from_json(inbound_payload, ddata)
+
+        byte_array = bytearray(inbound_payload.SerializeToString())
 
         print("Publishing DDATA")
-        infot = client.publish(topic, byteArray, qos=1)
-        # infot.wait_for_publish()
+        client.publish(topic, byte_array, qos=1)
 
         # Sit and wait for inbound or outbound events
         for _ in range(50):
@@ -72,7 +78,22 @@ def publishDData():
             client.loop()
 
 
+def __add_metric_from_json(inbound_payload, message_data):
+    for metric in message_data['metrics']:
+        addMetric(inbound_payload, metric['name'], metric['alias'], data_type_matching[metric['dataType']],
+                  metric['value'])
+
+
+def _get_message_data(file_name):
+    with open(file_name + '.json') as f:
+        data = json.load(f)
+    return data
+
+
 if __name__ == '__main__':
+    dbirth = _get_message_data(DBIRTH_FILE_NAME)
+    ddata = _get_message_data(DDATA_FILE_NAME)
+
     print("Start")
     client = mqtt.Client()
     client.on_connect = on_connect
@@ -85,7 +106,7 @@ if __name__ == '__main__':
     client.loop()
 
     # Publish the birth certificates
-    publishDeviceBirth()
-    
-    #Publish DDATA messages
-    publishDData()
+    publish_device_birth(dbirth)
+
+    # Publish DDATA messages
+    publish_ddata(ddata)
