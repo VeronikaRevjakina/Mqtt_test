10cce0f2d285197e81273860b8bd98930e3a2604
Veronika Revyakina <veronika.revyakina@syngenta.com>
2021-12-03 17:06:52 +0300

    Added influx file connection to local InfluxDB and reading table data with query

---
+++ influx.py
@@ -0,0 +1,22 @@
+from influxdb_client import InfluxDBClient
+
+my_token = ""
+my_org = ""
+
+if __name__ == '__main__':
+    client = InfluxDBClient(url="http://localhost:8086", token=my_token, org=my_org)
+    query_api = client.query_api()
+
+    query = 'from(bucket: "test")\
+  |>  range(start: -30d)\
+  |> filter(fn: (r) => r["_measurement"] == "mem")\
+  |> filter(fn: (r) => r["_field"] == "available" or r["_field"] == "available_percent" or r["_field"] == "total")'
+
+    result = client.query_api().query(org=my_org, query=query)
+
+    results = []
+    for table in result:
+        for record in table.records:
+            results.append((record.get_value(), record.get_field()))
+
+    print(results)
